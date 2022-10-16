1e54c230a598544104886953e5d61be121a54b1e
Иван Провоторов <ivanpro2010@yandex.ru>
2022-10-16 14:33:32 +0300

    Задача №2

---
+++ №2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, 
+//какое число большее, а какое меньшее.
+//Console.WriteLine("Hello, World!");
+Console.Clear();
+Console.WriteLine("Программа для определения большего числа");
+Console.WriteLine("Введите первое число: ");
+int numbera = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberb = Convert.ToInt32(Console.ReadLine());;
+int max = 0;
+if (numbera == numberb)
+{
+    Console.Write("числа равны");
+}
+else
+{
+    if (numbera > numberb) max = numbera ;
+    else max = numberb;
+    Console.Write("большее число: ");
+    Console.Write(max);
+}
