91cc57743a6385bf6f82b4827bbe77c7291b5c81
Иван Провоторов <ivanpro2010@yandex.ru>
2022-10-16 16:14:13 +0300

    Задача №8

---
+++ №8/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N),
+//а на выходе показывает все четные числа от 1 до (N).
+Console.Clear();
+Console.WriteLine("Программа для определения всех четных чисел от 1 до заданного (работает с целыми числами)");
+Console.Write("Задайте число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+while (i < number - 1)
+{
+    i +=2;
+    Console.Write(i);
+    Console.Write(",");
+}
